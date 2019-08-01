075a5f46fb07f483d8bb60a6ce9657d15829258a
Bruno Felipe Leal Delfino <bruno.delfino1995@gmail.com>
2019-08-01 19:10:39 -0300

    Configure functions with action name only once

--- src/actions.spec.js
+++ src/actions.spec.js
@@ -1,37 +1,25 @@
-import { STATUS, reset, pending, fulfilled, rejected } from './actions'
+import factory, { STATUS } from './actions'
 
 describe('actions', () => {
   const name = 'NAME'
 
-  // I know the action type could be just this, however, the actions logged on
-  // redux-dev-tools would require us to see its content to know the real intent.
-  // By prefixing the name with `@@redux-async` I scope the actions, and by
-  // appending the status I give them meaning.
   it('has type prefixed with @@redux-async/NAME', () => {
-    const { type: tReset } = reset(name)
-    const { type: tPending } = pending(name)
-    const { type: tFulfilled } = fulfilled(name, null)
-    const { type: tRejected } = rejected(name, null)
+    const actions = factory(name)
+    const types = Object.values(actions)
+      .map(ac => ac().type)
 
-    const types = [tReset, tPending, tFulfilled, tRejected]
     const prefix = new RegExp(`^@@redux-async/${name}`)
 
     types.forEach(el => expect(el).to.match(prefix))
   })
 
   it('has status under `meta`', () => {
-    const { meta: mReset } = reset(name)
-    const { meta: mPending } = pending(name)
-    const { meta: mFulfilled } = fulfilled(name, null)
-    const { meta: mRejected } = rejected(name, null)
+    const actions = factory(name)
+    const actionStatuses = Object.values(actions)
+      .map(ac => ac().meta)
+      .map(m => m.status)
+    const statuses = Object.values(STATUS)
 
-    const pairs = [
-      [mReset, STATUS.IDLE],
-      [mPending, STATUS.PENDING],
-      [mFulfilled, STATUS.FULFILLED],
-      [mRejected, STATUS.REJECTED],
-    ]
-
-    pairs.forEach(([meta, status]) => expect(meta.status).to.be.equal(status))
+    expect(actionStatuses).to.be.like(statuses)
   })
 })
--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
+import actions from './actions'
 import createAC from './createAC'
 import createR from './createR'
 
-export {
-  createAC,
-  createR,
+const factory = (name, { creator, reducer }) => {
+  const asyncCreator = createAC(name, creator)
+  const asyncReducer = createR(name, reducer)
+
+  return {
+    actions,
+    creator: asyncCreator,
+    reducer: asyncReducer,
+  }
 }
+
+export default factory
