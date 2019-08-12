34634688dec3daaffc0e95ae1080546038a60a5a
Bruno Felipe Leal Delfino <bruno.delfino1995@gmail.com>
2019-08-12 19:41:33 -0300

    Bind name on creators instead of createC

--- src/actions.js
+++ src/actions.js
@@ -6,23 +6,23 @@ import { STATUS, prefix } from './constants'
 // actions, and by appending the status I give them meaning.
 const createAT = (name, status) => `${prefix}/${name}/${status}`
 
-const reset = name => ({
+const reset = name => () => ({
   type: createAT(name, STATUS.IDLE),
   meta: { name, status: STATUS.IDLE },
 })
 
-const pending = name => ({
+const pending = name => () => ({
   type: createAT(name, STATUS.PENDING),
   meta: { name, status: STATUS.PENDING },
 })
 
-const fulfilled = (name, payload) => ({
+const fulfilled = name => payload => ({
   type: createAT(name, STATUS.FULFILLED),
   payload,
   meta: { name, status: STATUS.FULFILLED },
 })
 
-const rejected = (name, error) => ({
+const rejected = name => error => ({
   type: createAT(name, STATUS.REJECTED),
   payload: error,
   error: true,
@@ -30,10 +30,10 @@ const rejected = (name, error) => ({
 })
 
 const createC = name => ({
-  reset: () => reset(name),
-  execute: () => pending(name),
-  resolve: payload => fulfilled(name, payload),
-  reject: error => rejected(name, error),
+  reset: reset(name),
+  execute: pending(name),
+  resolve: fulfilled(name),
+  reject: rejected(name),
 })
 
 export default createC
